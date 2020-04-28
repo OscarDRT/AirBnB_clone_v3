a967f08005488755debc973d7afd5dece1f094d4
OscarDRT <oscarnetworkingpro@gmail.com>
2020-04-28 17:00:14 -0500

    changes

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify, abort, make_response, request
 from api.v1.views import app_views
 from models.state import *
 from models import storage
+from base_model import *
 
 
 @app_views.route('/states/', methods=['POST', 'GET'])
@@ -19,13 +20,12 @@ def states():
     if request.method == 'POST':
         data = request.get_json()
         if data is None:
-            return (jsonify({"error": "Not a JSON"}), 400)
-        if 'name' in data:
+            return make_response(jsonify({'error': 'Not a Json'}), 400)
+        elif 'name' in data:
             state = State(**data)
             state.save()
             return make_response(jsonify(data), 201)
-        return (jsonify({"error": "Missing name"}), 400)
-
+        return make_response(jsonify({'error': 'Missing name'}), 400)
 
 @app_views.route('/states/<state_id>', methods=['DELETE', 'GET', 'PUT'])
 def state(state_id):
@@ -44,14 +44,14 @@ def state(state_id):
         return make_response(jsonify({}), 200)
 
     if request.method == 'PUT':
-        data = request.get_json()
-        if data is None:
-            return (jsonify({"error": "Not a JSON"}), 400)
+        if not request.get_json():
+            return make_response(jsonify({'error': 'Not a Json'}), 400)
         if state is None:
             abort(404)
+        data = request.get_json()
         ignorekey = ['id', 'created_at', 'updated_at']
         for key, value in data.items():
             if key not in ignorekey:
                 setattr(state, key, value)
         state.save()
-        return jsonify(state.to_dict())
+        return make_response(jsonify(state.to_dict()), 200)
