e76f890487006fb28b7a7d699031d02ee5dcb8c5
OscarDRT <oscarnetworkingpro@gmail.com>
2020-04-28 15:13:08 -0500

    Fix pep8 stylecode

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""Create app views"""
 from flask import Blueprint
 
-"""Create app views"""
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
 
 from api.v1.views.index import *
