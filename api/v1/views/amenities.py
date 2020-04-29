4a7a9836f4457819f48d4c00f14db696ec7f8a4a
OscarDRT <oscarnetworkingpro@gmail.com>
2020-04-28 21:27:50 -0500

    add amenities api

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -27,7 +27,7 @@ def amenities_list():
             amenity = Amenity(**data)
             amenity.save()
             data2 = storage.get(Amenity, amenity.id).to_dict()
-            return jsonify(amenity.to_dict()), 201
+            return jsonify(data2), 201
         return (jsonify({"error": "Missing name"}), 400)
 
 
