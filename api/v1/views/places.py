e9e485f7123ae41d355be4cc50258c086c8c316c
Emmus Monsalve <emonsalvep38@gmail.com>
2020-04-29 10:48:12 -0500

    Add somethings methods to class Place

---
+++ api/v1/views/places.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""Documentation"""
+from flask import Flask, jsonify, abort, make_response, request
+from api.v1.views import app_views
+from models.city import *
+from models import storage
+
+
+@app_views.route('/cities/<city_id>/places', methods=['GET', 'POST'],
+                 strict_slashes=False)
+def places_li(city_id):
+    """Places"""
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+
+    if request.method == 'GET':
+        places_list = []
+
+        for key, value in storage.all('Place').items():
+            if value.state_id == str(state_id):
+                places_list.append(value.to_dict())
+        return jsonify(cities_list)
+
+    if request.method == 'POST':
+        data = request.get_json()
+        if data is None:
+            return (jsonify({"error": "Not a JSON"}), 400)
+        if 'user_id' not in data:
+            return (jsonify({"error": "Missing user_id"}), 400)
+        if 'name' not in data: 
+            return (jsonify({"error": "Missing name"}), 400)
+        if storage.get(User, data['user_id']) is None:
+            abort(404)
+        place = Place(**data)
+        place.save()
+        return make_response(jsonify(place.to_dict()), 201)        
+
+@app_views.route('/places/<place_id>', methods=['GET', 'DELETE', 'PUT'],
+                 strict_slashes=False)
+def my_places(place_id):
+    """places"""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+
+    if request.method == 'GET':
+        return jsonify(place.to_dict())
+
+    if request.method == 'DELETE':
+        storage.delete(place)
+        storage.save()
+        return jsonify({}), 200
+
+    if request.method == 'PUT':
+        data = request.get_json()
+        if data is None:
+            return (jsonify({"error": "Not a JSON"}), 400)
+        ignorekey = ['id', 'user_id', 'city_id', 'created_at', 'updated_at']
+        for key, value in data.items():
+            if key not in ignorekey:
+                setattr(place, key, value)
+        place.save()
+        return jsonify(place.to_dict()), 200
